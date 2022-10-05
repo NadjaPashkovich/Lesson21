49d8f3f314ee6a61c4f91a5d7fbdc64f98a18446
NadjaPashkovich <nadjapashka@gmail.com>
2022-10-05 23:25:44 +0300

    initial

--- src/by/itstep/pashka/model/logic/ArraySorter.java
+++ src/by/itstep/pashka/model/logic/ArraySorter.java
@@ -1,22 +1,80 @@
 package by.itstep.pashka.model.logic;
 
 public class ArraySorter {
-    public static void bubbleSortAsc(int[ ] array){
+    public static void selectedSortAsc(int[] array) {
+
+        if (array == null) {
+            throw new RuntimeException();
+        }
+        for (int j = 0; j < array.length - 1; j++) {
+
+            int indexMinElement = j;
+            for (int i = j + 1; i < array.length; i++) {
+                if (array[indexMinElement] > array[i]) {
+                    indexMinElement = i;
+                }
+            }
+            int t = array[j];
+            array[j] = array[indexMinElement];
+            array[indexMinElement] = t;
+        }
+    }
+
+    public static void selectedSortDesc(int[] array) {
+        if (array == null) {
+            throw new RuntimeException();
+        }
+        for (int j = 0; j < array.length - 1; j++) {
+
+            int indexMaxElement = j;
+            for (int i = j + 1; i < array.length; i++) {
+                if (array[indexMaxElement] < array[i]) {
+                    indexMaxElement = i;
+                }
+            }
+            int t = array[j];
+            array[j] = array[indexMaxElement];
+            array[indexMaxElement] = t;
+        }
+    }
+
+
+    public static void bubbleSortAsc(int[] array) {
         int count = 0;
-        if (array == null){
+        if (array == null) {
             throw new RuntimeException();
         }
 
-        for (int j = 0; j <array.length -1 ; j++) {
-        for (int i = 0; i < array.length - 1; i++) {
-            count++;
-            if (array[i] > array[i + 1]) {
-                int t = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = t;
+        for (int j = 0; j < array.length - 1; j++) {
+            for (int i = 0; i < array.length - 1 - j; i++) {
+                count++;
+                if (array[i] > array[i + 1]) {
+                    int t = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = t;
 
+                }
             }
         }
+        System.out.println("count = " + count);
+    }
+
+    public static void bubbleSortDesc(int[] array) {
+        int count = 0;
+        if (array == null) {
+            throw new RuntimeException();
+        }
+
+        for (int j = 0; j < array.length - 1; j++) {
+            for (int i = 0; i < array.length - 1 - j; i++) {
+                count++;
+                if (array[i] < array[i + 1]) {
+                    int t = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = t;
+
+                }
+            }
         }
         System.out.println("count = " + count);
     }
--- test/by/itstep/pashka/model/logic/ArraySorterTest.java
+++ test/by/itstep/pashka/model/logic/ArraySorterTest.java
@@ -1,22 +1,38 @@
 package by.itstep.pashka.model.logic;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ArraySorterTest {
+
+    private int[] array;
+
+    @Before
+    public void init() {
+        array = new int[]{5, 4, 3};
+    }
+
+    @After
+    public void destroy() {
+        array = null;
+    }
+
     @Test
     public void testBubbleSortAsc() {
-        int[] array = {5, 4, 3};
+
         int[] expected = {3, 4, 5};
 
         ArraySorter.bubbleSortAsc(array);
+        assertArrayEquals(expected, array);
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != expected[i]) {
-                fail();
-            }
-        }
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] != expected[i]) {
+//                fail();
+//            }
+//        }
     }
 
     @Test(expected = RuntimeException.class)
@@ -27,5 +43,77 @@ public class ArraySorterTest {
     @Test
     public void testBubbleSortWitEmptyObject() {
         ArraySorter.bubbleSortAsc(new int[0]);
+    }
+
+
+    @Test
+    public void testSelectedSortAsc() {
+
+        int[] expected = {3, 4, 5};
+
+        ArraySorter.selectedSortAsc(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != expected[i]) {
+                fail();
+            }
+        }
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testSelectedSortWithNull() {
+        ArraySorter.selectedSortAsc(null);
+    }
+
+    @Test
+    public void testSelectedSortWitEmptyObject() {
+        ArraySorter.selectedSortAsc(new int[0]);
+    }
+
+
+    @Test
+    public void testSelectedSortDesc() {
+
+        int[] expected = {5, 4, 3};
+
+        ArraySorter.selectedSortDesc(array);
+        assertArrayEquals(expected, array);
+
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testSelectedSortDescWithNull() {
+        ArraySorter.selectedSortDesc(null);
+    }
+
+    @Test
+    public void testSelectedSortDescWitEmptyObject() {
+        ArraySorter.selectedSortDesc(new int[0]);
+    }
+
+
+    @Test
+    public void testBubbleSortDesc() {
+
+        int[] expected = {5, 4, 3};
+
+        ArraySorter.bubbleSortDesc(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != expected[i]) {
+                fail();
+            }
+        }
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testBubbleSortDescWithNull() {
+        ArraySorter.bubbleSortDesc(null);
+    }
+
+    @Test
+    public void testBubbleSortDescWitEmptyObject() {
+        ArraySorter.bubbleSortDesc(new int[0]);
     }
 }
